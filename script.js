780a7e5cd03f1371e8143c54e778a0f9bf405646
CloudYR-hub <rokaszuju@gmail.com>
2026-08-11 16:43:01 +0300

    Restarted project and added the button to change grid size

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let container = document.querySelector('.container');
-
-
+let containerWidth = container.offsetWidth;
+let containerHeight = container.offsetHeight;
+let size = 6;
 function draw(size){
     for(let i = 1; i <= size * size; i++){
         let box = document.createElement('div');
         box.classList.add('box');
         container.appendChild(box);
+        box.style.width = containerWidth / size + "px";
+        box.style.height = containerHeight / size + "px";
+        box.addEventListener('mouseenter', (event)=>{
+            box.style.backgroundColor = "black";
+        })
+        
     }
 }
-draw(16);
+let button = document.getElementById("button");
+button.addEventListener('click', (event)=>{
+    size = Number(prompt("Choose grid size", "0"));
+    container.replaceChildren();
+    draw(size);
+})
+draw(size);
