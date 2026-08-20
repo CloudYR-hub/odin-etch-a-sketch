b382269615d7b9334a81b694da08dbfd721e8c81
Rokas Zujus <r.zujus@cognizant.com>
2026-08-20 14:14:04 +0300

    Added a limit to the prompt

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-let container = document.querySelector('.container');
+let container = document.querySelector(".container");
 let containerWidth = container.offsetWidth;
 let containerHeight = container.offsetHeight;
 let size = 6;
-function draw(size){
-    for(let i = 1; i <= size * size; i++){
-        let box = document.createElement('div');
-        box.classList.add('box');
-        container.appendChild(box);
-        box.style.width = containerWidth / size + "px";
-        box.style.height = containerHeight / size + "px";
-        box.addEventListener('mouseenter', (event)=>{
-            box.style.backgroundColor = "black";
-        })
-        
-    }
+function draw(size) {
+  for (let i = 1; i <= size * size; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    container.appendChild(box);
+    box.style.width = containerWidth / size + "px";
+    box.style.height = containerHeight / size + "px";
+    box.addEventListener("mouseenter", (event) => {
+      box.style.backgroundColor = "black";
+    });
+  }
 }
 let button = document.getElementById("button");
-button.addEventListener('click', (event)=>{
-    size = Number(prompt("Choose grid size", "0"));
+button.addEventListener("click", (event) => {
+  size = Number(prompt("Choose grid size", "0"));
+  if (size >= 100) {
+    return;
+  } else {
     container.replaceChildren();
     draw(size);
-})
+  }
+});
 draw(size);
